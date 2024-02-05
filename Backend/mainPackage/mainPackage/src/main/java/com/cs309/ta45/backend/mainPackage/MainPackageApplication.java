b22fd6e6ae0bef79183c7351f2f30a13b8cde631
Junhyung Shim <jshim@iastate.edu>
2024-02-04 21:52:08 -0600

    implemented ConnectToDB. Referenced mysql docs to figure out how jdbc works

--- Backend/mainPackage/mainPackage/src/main/java/com/cs309/ta45/backend/mainPackage/MainPackageApplication.java
+++ Backend/mainPackage/mainPackage/src/main/java/com/cs309/ta45/backend/mainPackage/MainPackageApplication.java
@@ -1,13 +1,28 @@
 package com.cs309.ta45.backend.mainPackage;
 
+import com.cs309.ta45.backend.dbmsPackage.ConnectToDB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+
 
 @SpringBootApplication
 public class MainPackageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainPackageApplication.class, args);
+
+		Connection connection =  null; //According to google: no multiple sessions over a single connection is allowed.
+		ConnectToDB db = null;
+		try{
+			connection = ConnectToDB.getOneTimeConnection();
+			connection.close();
+			SpringApplication.run(MainPackageApplication.class, args);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
