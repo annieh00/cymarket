f1622c0c8fce2e6e2d9b54ba708362511f77f3c5
Junhyung Shim <jshim@iastate.edu>
2024-02-04 21:02:12 -0600

    created new project using spring initializer with dbms, websocket, spring-web

---
+++ Backend/mainPackage/mainPackage/src/main/java/com/cs309/ta45/backend/mainPackage/MainPackageApplication.java
@@ -0,0 +1,13 @@
+package com.cs309.ta45.backend.mainPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainPackageApplication.class, args);
+	}
+
+}
---
+++ Backend/mainPackage/mainPackage/src/test/java/com/cs309/ta45/backend/mainPackage/MainPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs309.ta45.backend.mainPackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
