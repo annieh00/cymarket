1573d11d0f876aec359d234120302cdac06fc6ba
Annie Huynh <annieh47@iastate.edu>
2024-02-27 00:34:23 -0600

    added successful login and registration round trips (i think)

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/CreatePostActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/CreatePostActivity.java
@@ -1,16 +1,297 @@
 package com.example.androidexample;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CreatePostActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import org.json.JSONArray;
+
+import com.android.volley.toolbox.Volley;
+import com.example.androidexample.Const;
+import com.example.androidexample.Manifest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreatePostActivity extends AppCompatActivity{
     private EditText titleEditText;
     private ImageButton addImageBtn;
     private EditText descriptionEditText;
     private Button cancelBtn;
     private Button postBtn;
+    private String TAG = CreatePostActivity.class.getSimpleName();
+    private EditText category;
+    private HorizontalScrollView images;
+    private ImageView image1 = null;
+    private ImageView image2 = null;
+    private ImageView image3 = null;
+    private ImageView image4 = null;
+    private ImageView image5 = null;
+    private ImageView image6 = null;
+    private static final String ROOT_URL = "http://seoforworld.com/api/v1/file-upload.php";
+    private static final int REQUEST_PERMISSIONS = 100;
+    private static final int PICK_IMAGE_REQUEST =1 ;
+    private Bitmap bitmap;
+    private String filePath;
+//    ImageView imageView;
+    TextView textView;
 
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);            // link to Login activity XML
+
+        /* initialize UI elements */
+        //Text
+
+        titleEditText = findViewById(R.id.Title);
+        descriptionEditText = findViewById(R.id.DescriptionEditText);
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        image5 = findViewById(R.id.image5);
+        image6 = findViewById(R.id.image6);
+
+
+
+        //Buttons
+        cancelBtn = findViewById(R.id.cancel_button);    // link to login button in the Login activity XML
+        postBtn = findViewById(R.id.post_button);  // link to signup button in the Login activity XML
+        addImageBtn = findViewById(R.id.addImageButton); //link to add images
+
+
+
+        /* click listener on signup button pressed */
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when post button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(CreatePostActivity.this, MainFeed.class);
+                startActivity(intent);  // go to SignupActivity
+            }
+        });
+
+        addImageBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+//                if ((ContextCompat.checkSelfPermission(getApplicationContext(),
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(getApplicationContext(),
+//                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+//                    if ((ActivityCompat.shouldShowRequestPermissionRationale(CreatePostActivity.this,
+//                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(CreatePostActivity.this,
+//                            Manifest.permission.READ_EXTERNAL_STORAGE))) {
+//
+//                    } else {
+//                        ActivityCompat.requestPermissions(CreatePostActivity.this,
+//                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+//                                REQUEST_PERMISSIONS);
+//                    }
+//                } else {
+//                    Log.e("Else", "Else");
+//                    showFileChooser();
+//                }
+
+
+            }
+        });
+
+        /* click listener on login button pressed */
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePostActivity.this, MainFeed.class);
+                startActivity(intent);  // go to SignupActivity
+            }
+        });
+    }
+
+    private void sendJsonObjReq() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JSONObject jsonObject = new JSONObject();
+        JSONObject body = new JSONObject();
+        try {
+            //input your API parameters
+            jsonObject.put("title", titleEditText.getText().toString());
+            jsonObject.put("description", descriptionEditText.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, Const.URL_POST_LOGIN_USER, jsonObject, response -> {
+            Log.d(TAG, response.toString());
+            try {
+//                email = response.getString("email");
+//                password = response.getString("password");
+//                validUser = response.getBoolean("fromServer");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+
+        }, error -> {
+            VolleyLog.d(TAG, "Error: " + error.getMessage());
+            Toast.makeText(CreatePostActivity.this, "User Not Found", Toast.LENGTH_LONG).show();
+//            txtValidity = true;
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+
+        };
+
+        //queue.add(jsonObjReq);
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
+        //VolleySingleton.getInstance(getApplicationContext()).getRequestQueue().start();
+
+
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+//            Uri picUri = data.getData();
+//            filePath = getPath(picUri);
+//            if (filePath != null) {
+//                try {
+//
+//                    textView.setText("File Selected");
+//                    Log.d("filePath", String.valueOf(filePath));
+//                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), picUri);
+//                    uploadBitmap(bitmap);
+//                    imageView.setImageBitmap(bitmap);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            else
+//            {
+//                Toast.makeText(
+//                        CreatePostActivity.this,"no image selected",
+//                        Toast.LENGTH_LONG).show();
+//            }
+//        }
+//
+////    }
+//    public String getPath(Uri uri) {
+//        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+//        cursor.moveToFirst();
+//        String document_id = cursor.getString(0);
+//        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+//        cursor.close();
+//
+//        cursor = getContentResolver().query(
+//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+//                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+//        cursor.moveToFirst();
+//        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+//        cursor.close();
+//
+//        return path;
+//    }
+
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+//    private void uploadBitmap(final Bitmap bitmap) {
+//
+//        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, ROOT_URL,
+//                new Response.Listener<NetworkResponse>() {
+//                    @Override
+//                    public void onResponse(NetworkResponse response) {
+//                        try {
+//                            JSONObject obj = new JSONObject(new String(response.data));
+//                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+//                        Log.e("GotError",""+error.getMessage());
+//                    }
+//                }) {
+//
+//
+//            @Override
+//            protected Map<String, DataPart> getByteData() {
+//                Map<String, DataPart> params = new HashMap<>();
+//                long imagename = System.currentTimeMillis();
+//                params.put("image", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+//                return params;
+//            }
+//        };
+//
+//        //adding the request to volley
+//        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+//    }
 
 }
+
+
--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -1,29 +1,51 @@
 package com.example.androidexample;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import org.json.JSONArray;
+import com.example.androidexample.Const;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText usernameEditText;  // define username edittext variable
+    private EditText emailEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
     private EditText confirmEditText;   // define confirm edittext variable
     private Button loginButton;         // define login button variable
     private Button signupButton;        // define signup button variable
-
+    private String TAG = SignupActivity.class.getSimpleName(); //the tag used to identify JSON object requests
+    public String email;
+    public Boolean alreadyExists;
+    public String passsword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
         /* initialize UI elements */
-        usernameEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
+        emailEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
         passwordEditText = findViewById(R.id.signup_password_edt);  // link to password edtext in the Signup activity XML
         confirmEditText = findViewById(R.id.signup_confirm_edt);    // link to confirm edtext in the Signup activity XML
         loginButton = findViewById(R.id.signup_login_btn);    // link to login button in the Signup activity XML
@@ -46,17 +68,86 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 /* grab strings from user inputs */
-                String username = usernameEditText.getText().toString();
+                String username = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
                 String confirm = confirmEditText.getText().toString();
 
                 if (password.equals(confirm)){
                     Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+                    sendJsonObjReq();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    private void sendJsonObjReq() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            //input your API parameters
+            jsonObject.put("email", emailEditText.getText().toString());
+            jsonObject.put("password", passwordEditText.getText().toString());
+
+            //            Toast.makeText(LoginActivity.this, "got e and p", Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println(jsonObject.toString());
+//        String URL_POST_LOGIN_USER = "https://07537acc-da80-4457-8b10-ff9e97cbea07.mock.pstmn.io/user1";
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, Const.URL_POST_REGISTER_USER, jsonObject, response -> {
+            Log.d(TAG, response.toString());
+            try {
+//                    Toast.makeText(LoginActivity.this, "Successfully got email and password", Toast.LENGTH_LONG).show();
+//                    Toast.makeText(LoginActivity.this, "assigning values", Toast.LENGTH_LONG).show();
+//                email = response.getString("email");
+//                 = response.getString("password");
+                alreadyExists = response.getBoolean("fromServer");
+
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+                if (alreadyExists == true){
+                    Toast.makeText(SignupActivity.this, "User Already Exists", Toast.LENGTH_LONG).show();
+                }else{
+                    Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                    Toast.makeText(SignupActivity.this, "Login", Toast.LENGTH_LONG).show();
+                    startActivity(intent);
+                }
+
+
+//                    Toast.makeText(LoginActivity.this, "User Not Found", Toast.LENGTH_LONG).show();
+//                }
+
+        }, error -> {
+            VolleyLog.d(TAG, "Error: " + error.getMessage());
+//            Toast.makeText(SignupActivity.this, "User Not Found", Toast.LENGTH_LONG).show();
+        }) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+
+        };
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
+
+
     }
 }
