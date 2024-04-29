a74524d99e7aff4ecaa99fb0623c998f24e09af9
Jessica Guerrero <jessg05@iastate.edu>
2024-04-29 17:25:19 -0500

    connected search to UI

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Fragment/RecentSearchesFragment.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Fragment/RecentSearchesFragment.java
@@ -1,14 +1,29 @@
 package com.example.androidexample.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.androidexample.LoginActivity;
 import com.example.androidexample.R;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,10 +31,72 @@ import com.example.androidexample.R;
  * create an instance of this fragment.
  */
 public class RecentSearchesFragment extends Fragment {
+
+    TextView clearAllSearches;
+
+    RequestQueue requestQueue;
+
+
+    private String URL = "http://coms-309-060.class.las.iastate.edu:8080";
+
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recent_searches, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_recent_searches, container, false);
+
+        // Initialize components
+        clearAllSearches = rootView.findViewById(R.id.clearAllTextView);
+
+        requestQueue = Volley.newRequestQueue(requireContext());
+
+
+        // Set listeners or perform any other setup
+        clearAllSearches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearSearches();
+            }
+
+        });
+
+        return rootView;
+
+
     }
+
+    private void clearSearches() {
+
+        // Make network request
+        String url = URL + "/search/" + LoginActivity.loginID + "/clear";
+
+        StringRequest request = new StringRequest(Request.Method.DELETE, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Delete Search", response.toString());
+
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Did not work", error.toString());
+                    }
+                });
+
+        // Add the request to the RequestQueue
+
+        Volley.newRequestQueue(requireContext()).add(request);
+
+
+
+
+    }
+
 }
--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/SearchActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/SearchActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -11,31 +16,39 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.androidexample.Fragment.RecentSearchesFragment;
 import com.google.android.material.tabs.TabLayout;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SearchActivity extends AppCompatActivity {
-
     TabLayout tabLayout;
     ViewPager2 viewPager2;
-
     SearchActivityTabAdapter searchActivityTabAdapter;
-
+    ImageButton sendSearch;
+    EditText searchInquiry;
+    TextView clearHistory;
 
     private String URL = "http://coms-309-060.class.las.iastate.edu:8080";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        sendSearch = findViewById(R.id.searchBtn);
+        searchInquiry = findViewById(R.id.searchItem);
         tabLayout = findViewById(R.id.tabLayout);
         viewPager2 = findViewById(R.id.view_pager);
         searchActivityTabAdapter = new SearchActivityTabAdapter(this);
         viewPager2.setAdapter(searchActivityTabAdapter);
+
+
+
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -55,15 +68,64 @@ public class SearchActivity extends AppCompatActivity {
         });
 
 
+        sendSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //making sure that search inquiry actually has something
+                if(searchInquiry.toString() != null){
+
+                    searchRequest();
+
+
+                }
+            }
+        });
         pullRecentSearches();
 
     }
 
+    private void searchRequest() {
+        // URL of your backend API
+        String url = URL + "/search/" + LoginActivity.loginID + "?query=" + searchInquiry.getText().toString();
+        //maybe this idk yet
+        // url:8080/search/3?query=""
+        // Create JSONObject for parameters
+        JSONObject jsonBody = new JSONObject();
+        try {
+            // Add query parameter
+            jsonBody.put("query", searchInquiry.getText().toString());
+            // Add any other parameters if required
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Create request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("SearchRequest", response.toString());
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error here
+                        Log.d("SearchRequest", "Search successful");
+
+                    }
+                });
+
+        // Add the request to the RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+    }
     private void pullRecentSearches() {
 
         //this method needs work, im not sure for the endpoint yet or the structure of the announcements
 
-        String friends_url = URL + "/friends/" + LoginActivity.loginID + "/list";
+        String friends_url = URL + "/search/" + LoginActivity.loginID + "/history";
 //        String friends_url = URL + "/friendrequests/" + LoginActivity.username + "/";
 //        String friends_url = "https://37668f7b-a5c8-475c-821b-06324c4610a1.mock.pstmn.io" + "/friends";
         //replace userName with LoginActivity.username
@@ -75,26 +137,19 @@ public class SearchActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONArray response) {
 
+                        Log.d("Previous Searches", response.toString());
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // Handle errors
-                        Toast.makeText(SearchActivity.this, "Error fetching recent searches", Toast.LENGTH_SHORT).show();
+                        Log.d("Error getting previous searches", error.toString());
                     }
                 });
 
         queue.add(jsonArrayRequest);
 
-
-
-
-
-
-
-
     }
-
 
 }
